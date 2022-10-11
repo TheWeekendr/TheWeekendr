14c60c56996964a651fae93f421a11f95f2f32f3
dustinapodaca <dustinapodaca@gmail.com>
2022-10-11 10:15:51 -0600

    updated sidenav

--- src/components/Dash.js
+++ src/components/Dash.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 class Dash extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+
+
   render() {
     return (
       <>
