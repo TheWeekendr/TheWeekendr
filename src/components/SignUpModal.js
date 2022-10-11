36c4d72f067691acc43906ada5d868ea87cef964
Chris Fanucchi <admin@knightskeep.com>
2022-10-11 12:23:09 -0700

    BAsic add user functionworking

--- src/components/SignUpModal.js
+++ src/components/SignUpModal.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Modal, Button } from 'rsuite';
 import { Form, FormGroup, FormControl, ControlLabel, HelpBlock } from 'rsuite';
 import { CheckPicker } from 'rsuite';
-
+import axios from 'axios';
 
 class SignUpModal extends React.Component {
   constructor(props) {
@@ -10,17 +10,18 @@ class SignUpModal extends React.Component {
     this.state = {
       formValue: {
         name: '',
-        email: '',
         password: '',
         zipCode: '',
-        favFoods: [],
-        favActivities: [],
       },
-      show: false
+      favFoods: [],
+      favActivities: [],
+      show: false,
     };
     this.close = this.close.bind(this);
     this.open = this.open.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleFoods = this.handleFoods.bind(this);
+    this.handleActivities = this.handleActivities.bind(this);
   }
   close() {
     this.setState({ show: false });
@@ -30,12 +31,96 @@ class SignUpModal extends React.Component {
   }
   handleChange(value) {
     this.setState({
-      formValue: value
+      formValue: value,
     });
   }
+
+  handleFoods(value) {
+    this.setState({
+      favFoods: value,
+    });
+  }
+
+  handleActivities(value) {
+    this.setState({
+      favActivities: value,
+    });
+  }
+
+  handleCreateAccount = async () => {
+    let userData = {
+      name: this.state.formValue.name,
+      // password: this.state.formValue.password,
+      zipCode: this.state.formValue.zipCode,
+      favFoods: this.state.favFoods,
+      favActivities: this.state.favActivities,
+    };
+    try {
+      // const res = await axios.post(
+      //   `${process.env.REACT_APP_SERVER}/user`,
+      //   userData
+      // );
+      const res = await axios.post(`http://localhost:3001/user`, userData);
+
+      this.close();
+    } catch (error) {
+      alert(`Error: ${error.code} - ${error.message}`);
+    }
+  };
+
   render() {
+    const foodData = [
+      {
+        value: 'chinese',
+        label: 'Chinese',
+      },
+      {
+        value: 'thai',
+        label: 'Thai',
+      },
+      {
+        value: 'italian',
+        label: 'Italian',
+      },
+      {
+        value: 'mexican',
+        label: 'Mexican',
+      },
+      {
+        value: 'mongolian',
+        label: 'Mongolian',
+      },
+    ];
+
+    const activitiesData = [
+      {
+        value: 'golf',
+        label: 'Golf',
+      },
+      {
+        value: 'comedy',
+        label: 'Comedy',
+      },
+      {
+        value: 'wine',
+        label: 'Wine',
+      },
+      {
+        value: 'beer',
+        label: 'Beer',
+      },
+      {
+        value: 'art',
+        label: 'Art',
+      },
+      {
+        value: 'music',
+        label: 'Music',
+      },
+    ];
+
     return (
-      <div className='mt-4 mx-auto flex flex-col mx-16'>
+      <div className="mt-4 mx-auto flex flex-col mx-16">
         <Modal show={this.state.show} onHide={this.close} size="xs">
           <Modal.Header>
             <Modal.Title>Sign Up</Modal.Title>
@@ -52,40 +137,50 @@ class SignUpModal extends React.Component {
                 <HelpBlock>Required</HelpBlock>
               </FormGroup>
               <FormGroup>
-                <ControlLabel>Email</ControlLabel>
-                <FormControl name="email" type="email" />
+                <ControlLabel>Password</ControlLabel>
+                <FormControl name="password" type="password" />
                 <HelpBlock>Required</HelpBlock>
               </FormGroup>
               <FormGroup>
-                <ControlLabel>Password</ControlLabel>
-                <FormControl name="password" type="password" />
-              </FormGroup>
-              <FormGroup>
                 <ControlLabel>Zip Code</ControlLabel>
-                <FormControl name="zip-code" type="zip-code" />
+                <FormControl name="zipCode" type="zip-code" />
+                <HelpBlock>Required</HelpBlock>
               </FormGroup>
               <FormGroup>
-                <CheckPicker placeholder="Favorite Foods" style={{ width: 400 }} />
+                <CheckPicker
+                  onChange={this.handleFoods}
+                  data={foodData}
+                  placeholder="Favorite Foods"
+                  style={{ width: 400 }}
+                  preventOverflow
+                />
               </FormGroup>
               <FormGroup>
-                <CheckPicker placeholder="Favorite Activities" style={{ width: 400 }} />
+                <CheckPicker
+                  onChange={this.handleActivities}
+                  data={activitiesData}
+                  placeholder="Favorite Activities"
+                  style={{ width: 400 }}
+                  preventOverflow
+                />
               </FormGroup>
             </Form>
           </Modal.Body>
           <Modal.Footer>
-            <Button onClick={this.close} appearance="primary">
-              Confirm
+            <Button onClick={this.handleCreateAccount} appearance="primary">
+              Create Account
             </Button>
             <Button onClick={this.close} appearance="subtle">
               Cancel
             </Button>
           </Modal.Footer>
         </Modal>
-        <Button appearance="ghost" onClick={this.open}>Sign Up</Button>
+        <Button appearance="ghost" onClick={this.open}>
+          Sign Up
+        </Button>
       </div>
     );
   }
 }
-
 
 export default SignUpModal;
