8e8566902cfe947e53e1d2f9a8840547dfc270da
Mathew Torres <torres.mathew@gmail.com>
2022-10-12 11:13:05 -0700

    added some auth0 stuff

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <Auth0Provider
+    domain={process.env.REACT_APP_DOMAIN}
+    clientId={process.env.REACT_APP_CLIENTID}
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>
+
+);
 
