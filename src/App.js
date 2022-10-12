5b875c82c9b5adc255175d212f0920c7690b88c0
Chris Fanucchi <admin@knightskeep.com>
2022-10-11 17:32:38 -0700

    Added user record update functionality and getting current user from DB

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import Layout from './components/Layout';
 import DispLayout from './components/DispLayout';
 import './App.css';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      userData: {},
+      userData: {
+        name: '',
+        password: '',
+        zipCode: '',
+        favFoods: [],
+        favActivities: [],
+      },
     };
   }
 
@@ -15,6 +22,20 @@ class App extends React.Component {
     this.setState({
       userData: data,
     });
+  };
+
+  getUser = async data => {
+    try {
+      // let userData = await axios.get(`${process.env.REACT_APP_SERVER}/user-name/Alice`);
+      // let userData = await axios.get(`http://localhost:3001/user-name/Alice`);
+      let userData = await axios.get(
+        `http://localhost:3001/user-name/${data.name}`
+      );
+
+      this.setState({ userData: userData.data[0] });
+    } catch (error) {
+      alert(`Error: ${error.code} - ${error.message}`);
+    }
   };
 
   render() {
@@ -28,6 +49,7 @@ class App extends React.Component {
         <DispLayout
           userData={this.state.userData}
           setUserDataState={this.setUserDataState}
+          getUser={this.getUser}
         />
       </>
     );
